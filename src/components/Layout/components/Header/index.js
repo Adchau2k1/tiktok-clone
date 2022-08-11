777387bdc2c77bef8cb4361245f37877ae14131b
Adchau2k1 <adchau2k1@gmail.com>
2022-08-12 03:25:02 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,30 +1,40 @@
 import { Link } from 'react-router-dom'
+import className from 'classnames/bind'
+import styles from './Header.module.scss'
+
+const cx = className.bind(styles)
 
 function Header() {
 	return (
-		<header>
-			<nav
-				style={{
-					display: 'flex',
-					listStyle: 'none',
-					backgroundColor: 'pink',
-				}}
-			>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li style={{ marginLeft: 10 }}>
-					<Link to="/search">Search</Link>
-				</li>
-				<li style={{ marginLeft: 10 }}>
-					<Link to="/following">Follow</Link>
-				</li>
-				<li style={{ marginLeft: 10 }}>
-					<Link to="/upload">Upload</Link>
-				</li>
-				<li style={{ marginLeft: 10 }}>
-					<Link to="/profile">Profile</Link>
-				</li>
+		<header className={cx('wrapper')}>
+			<nav className={cx('inner')}>
+				<ul className={cx('nav')}>
+					<li>
+						<Link className={cx('nav__item')} to="/">
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link className={cx('nav__item')} to="/search">
+							Search
+						</Link>
+					</li>
+					<li>
+						<Link className={cx('nav__item')} to="/following">
+							Follow
+						</Link>
+					</li>
+					<li>
+						<Link className={cx('nav__item')} to="/upload">
+							Upload
+						</Link>
+					</li>
+					<li>
+						<Link className={cx('nav__item')} to="/profile">
+							Profile
+						</Link>
+					</li>
+				</ul>
 			</nav>
 		</header>
 	)
